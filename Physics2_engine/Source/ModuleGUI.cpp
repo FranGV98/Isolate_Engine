8e0895e4496bbd83d10902766932a668918aa05e
Turmo11 <Turmo11@users.noreply.github.com>
2020-10-06 21:37:37 +0200

    MathGeoLib added, not implemented yet

--- Physics2_engine/Source/ModuleGUI.cpp
+++ Physics2_engine/Source/ModuleGUI.cpp
@@ -5,7 +5,6 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
-#pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 
 ModuleGUI::ModuleGUI(bool start_enabled) : Module(start_enabled)
--- Physics2_engine/Source/ModuleRenderer3D.cpp
+++ Physics2_engine/Source/ModuleRenderer3D.cpp
@@ -7,7 +7,6 @@
 #include <gl/GL.h>
 #include <gl/GLU.h>
 
-#pragma comment (lib, "glu32.lib")    /* link OpenGL Utility lib     */
 #pragma comment (lib, "opengl32.lib") /* link Microsoft OpenGL lib   */
 #pragma comment (lib, "glew/libx86/glew32.lib")
 
